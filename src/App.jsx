2c6e7667d58170ae173bfd46e2eeba96600ffba5
surnaturel <numirix.yannick@gmail.com>
2023-10-24 12:36:41 +0000

    premier TP

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,86 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+];
 
 function App() {
-  // Utilisez le hook useState pour gérer l'état de "nb"
-  const [personne, setPersonne] = useState({
-    firstName : "Essoh",
-    lastName : "yannick",
-    age : 18
-  });
-  const [nb, setNb] = useState(0)
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={PRODUCTS} />
+    </div>
+  );
+}
 
-  function increment() {
-    console.log('Bonjour');
-    setPersonne({ ...personne, age : personne.age++});
-  }
+function SearchBar() {
+  return (
+    <div>
+      <input type="text" placeholder="Search..." />
+      <Checkbox />
+    </div>
+  );
+}
 
-  function incrementCont() {
-    console.log('Bonjour nb');
-    setNb(nb + 1);
-  }
+function Checkbox() {
+  return (
+    <div>
+      <input type="checkbox" name="checkbox" id="checkbox" />
+      N'afficher que les produits en stock
+    </div>
+  );
+}
+
+function ProductTable(props) {
+  const { products } = props;
+  const categories = [...new Set(products.map(product => product.category))];
 
   return (
-    <>
-      <h1>Compteur de l'age {personne.age} </h1>
-      <button onClick={increment}>Incrémenter</button><br /><br />
-      <button onClick={incrementCont}>IncrémenterCOUNT {nb}</button>
-    </>
+    <div>
+      {categories.map(category => (
+        <ProductCategoryRow key={category} category={category} products={products} />
+      ))}
+    </div>
+  );
+}
+
+function ProductCategoryRow(props) {
+  const { category, products } = props;
+  const filteredProducts = products.filter(product => product.category === category);
+
+  return (
+    <div>
+      <h2>{category}</h2>
+      <ProductRow products={filteredProducts} />
+    </div>
+  );
+}
+
+function ProductRow(props) {
+  const { products } = props;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Prix</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map(product => (
+          <tr key={product.name}>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
